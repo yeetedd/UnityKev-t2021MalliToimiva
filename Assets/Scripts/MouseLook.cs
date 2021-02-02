287f2d5308d3b849838bec7a92650ba23e5c510c
Harakki <pirminkki@gmail.com>
2021-02-02 13:57:24 +0200

    immitial commit

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    //Hiiren nopeus
+    public float mouseSensitivity = 100f;
+
+    //automaattisesti kääntää bodyä kun hiirtä käytetään
+    public Transform playerBody;
+
+    //miten paljon on kääntynyt x-akselin suhteen
+    private float xRotation = 0f;
+
+    float mouseX;
+    float mouseY;
+    float minXAngle = -90f;
+    float minYAngle = 90f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //asettaa kursorin oikeaan kohtaan näyttöä
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        //kääntää hiirtä
+        //delta time laskee kuinka kauna yhteen ruutuun menee
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;  //ylos ja alas katsominen
+        xRotation = Mathf.Clamp(xRotation, minXAngle, minYAngle);  //kaantaa kameraa ja estaa ylikaantymisen
+
+        Vector3 rotationInEuler = transform.rotation.eulerAngles;
+        transform.localRotation = Quaternion.Euler(xRotation, rotationInEuler.y, 0f); //asettaa hiiren sijaintiin
+
+        transform.Rotate(Vector3.up * mouseX);  //vasemmalle ja oikeille
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public GameObject camera;
+
+    public float speed = 8f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+    private bool isGrounded = true;
+
+    private Vector3 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //katsotaan koskeeko maata
+        //isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
+        
+        //Lasketaan mihin suuntaan on mentava
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        move = camera.transform.right * x + camera.transform.forward * z;
+
+        //kerrotaan etta on liikuttava
+        controller.Move(move * speed * Time.deltaTime);
+
+        //hypyn koodi
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isGrounded = false;
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            
+        }
+        velocity.y += gravity * Time.deltaTime;
+        
+        //putoaminen
+        controller.Move(velocity * Time.deltaTime);
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+}
