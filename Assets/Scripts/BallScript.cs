e53461f656d8f2c3028a74e46fd8a3c566946a74
Harakki <pirminkki@gmail.com>
2021-03-09 12:31:15 +0200

    Tarkoituksena opettaa seuraavana ase kun hyppy on fiksattu. Shaderi on vain extraa ja ylimääräistä :)

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 7f);
+    }
+
+
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public float throwForce = 10f;
+    public GameObject throwObjectPrefab;
+    Camera FPSCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FPSCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Throw();
+        }
+    }
+
+    private void Throw() 
+    {
+        GameObject apple = Instantiate(throwObjectPrefab, transform.position, Quaternion.identity);
+        Vector3 throwAngle = FPSCamera.transform.forward;
+        throwAngle.y += 0.6f;
+        throwAngle = throwAngle.normalized;
+     
+
+        apple.GetComponent<Rigidbody>().AddForce(throwAngle * throwForce, ForceMode.Impulse);
+    }
+
+}
+
